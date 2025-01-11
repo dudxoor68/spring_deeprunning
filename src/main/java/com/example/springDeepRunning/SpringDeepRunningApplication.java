396bb8513f91de0f2c38ddeb1b3158f8c9a0d6c4
jyt <dudxoor68@naver.com>
2025-01-11 17:53:01 +0900

    seeting

---
+++ src/main/java/com/example/springDeepRunning/SpringDeepRunningApplication.java
@@ -0,0 +1,13 @@
+package com.example.springDeepRunning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeepRunningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDeepRunningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springDeepRunning/SpringDeepRunningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springDeepRunning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeepRunningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
